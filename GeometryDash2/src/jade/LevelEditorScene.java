0ab7ef6bb2e8006bcdc8bd4a2895adbc5ab1dd5b
LounSeg <104449821+LounSeg@users.noreply.github.com>
2023-02-23 15:30:59 +0100

    Event Listeners

---
+++ GeometryDash2/src/jade/LevelEditorScene.java
@@ -0,0 +1,25 @@
+package jade;
+
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class LevelEditorScene extends Scene {
+    public LevelEditorScene(String name) {
+        super.Scene(name);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update(double dt) {
+        
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+
+    }
+}
--- GeometryDash2/src/jade/Window.java
+++ GeometryDash2/src/jade/Window.java
@@ -3,23 +3,46 @@ package jade;
 import util.Time;
 
 import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
+    public ML mouseListener;
+    public KL keyListener;
+
     private static Window window = null;
     private boolean isRunning = true;
+    private Scene currentScene = null;
 
     public Window() {
+        this.mouseListener = new ML();
+        this.keyListener = new KL();
+
         this.setSize(1280,720);
         this.setTitle("Geometry Dash");
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
+        this.addMouseListener(mouseListener);
+        this.addMouseMotionListener(mouseListener);
         this.setLocationRelativeTo(null);
     }
 
     public void init() {
+        changeScene(0);
+    }
 
+    public void changeScene(int scene) {
+        switch (scene) {
+            case 0:
+                currentScene = new LevelEditorScene("Level Editor");
+                break;
+            default:
+                System.out.println("Do not know what this scene is.");
+                currentScene = null;
+                break;
+        }
     }
 
     public static Window getWindow() {
@@ -31,7 +54,7 @@ public class Window extends JFrame implements Runnable {
     }
 
     public void update(double dt) {
-        System.out.println(dt);
+        currentScene.update(dt);
     }
 
     @Override
